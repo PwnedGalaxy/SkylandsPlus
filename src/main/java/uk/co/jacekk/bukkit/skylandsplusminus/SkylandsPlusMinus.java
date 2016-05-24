0774a226e80eb8ad2d290386f87e5d62f1ad9825
Dereku <dereku@default.im>
2016-05-25 04:44:51 +0700

    LOL

--- src/main/java/uk/co/jacekk/bukkit/skylandsplusminus/SkylandsPlusMinus.java
+++ src/main/java/uk/co/jacekk/bukkit/skylandsplusminus/SkylandsPlusMinus.java
@@ -1,6 +1,5 @@
 package uk.co.jacekk.bukkit.skylandsplusminus;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -15,34 +14,26 @@ import uk.co.jacekk.bukkit.skylandsplusminus.listeners.MobSpawnListener;
 import uk.co.jacekk.bukkit.skylandsplusminus.listeners.PhysicsListener;
 
 public class SkylandsPlusMinus extends JavaPlugin {
-    private PlayerMovementWorker playerMovementWorker;
-    private TimeSyncWorker timeSyncWorker;
+
     public List<Entity> fallingEntities = new ArrayList<Entity>();
     public Map<World, World> timeSyncTable = new HashMap<World, World>();
-	
-	public void onEnable(){
-		
-		if (true /*|| Config.PREVENT_SAND_FALLING*/){
-			this.getServer().getPluginManager().registerEvents(new PhysicsListener(this), this);
-		}
-		
-		if (true /*|| Config.RESTRICT_MOB_SPAWNING*/){
-			this.getServer().getPluginManager().registerEvents(new MobSpawnListener(), this);
-		}
-                
-            this.playerMovementWorker = new PlayerMovementWorker(this);
-            this.timeSyncWorker = new TimeSyncWorker(this);
-            this.getServer().getScheduler().scheduleSyncRepeatingTask(this, this.playerMovementWorker, 0, 1);
-            this.getServer().getScheduler().scheduleSyncDelayedTask(this, new WorldLoadWorker(this));
-            this.getServer().getScheduler().scheduleSyncRepeatingTask(this, this.timeSyncWorker, 0, 20);
-	}
-	
-	@Override
-	public ChunkGenerator getDefaultWorldGenerator(String worldName, String id){
-		if(id == null || id.isEmpty()){
-			id = "offset=0";
-		}
-		return new uk.co.jacekk.bukkit.skylandsplusminus.generation.ChunkGenerator(id);
-	}
-	
+
+    @Override
+    public void onEnable() {
+        this.getServer().getPluginManager().registerEvents(new PhysicsListener(this), this);
+        this.getServer().getPluginManager().registerEvents(new MobSpawnListener(), this);
+////        Temporal Crafting server 
+//        this.getServer().getScheduler().scheduleSyncRepeatingTask(this,  new PlayerMovementWorker(this), 0, 1);
+//        this.getServer().getScheduler().scheduleSyncDelayedTask(this, new WorldLoadWorker(this));
+//        this.getServer().getScheduler().scheduleSyncRepeatingTask(this, new TimeSyncWorker(this), 0, 20);
+    }
+
+    @Override
+    public ChunkGenerator getDefaultWorldGenerator(String worldName, String id) {
+        if (id == null || id.isEmpty()) {
+            id = "offset=0";
+        }
+        return new uk.co.jacekk.bukkit.skylandsplusminus.generation.ChunkGenerator(id);
+    }
+
 }
